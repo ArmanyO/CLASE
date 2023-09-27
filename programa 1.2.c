d6f77073331b43f28038a8a523eed3a936c3e4e0
ArmanyO <144954447+ArmanyO@users.noreply.github.com>
2023-09-27 15:02:17 -0400

    PROGRAMA 1.2

---
+++ programa 1.2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*A, B, C, D: variables de tipo entero. */
+void main (void)
+{
+    int A, B, C, D;
+    printf("4,5,6,7");
+    scanf ("%d %d %d %d" , &A, &B, &C, &D);
+    printf ("\n %d %d %d %d ", D, C, B, A);
+
+}
